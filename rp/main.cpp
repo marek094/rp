1982f24155ea8f931ff84ef6a12b1e7bbede2342
Marek Cerny <me@marekcerny.com>
2017-02-19 12:15:29 +0100

    Initial Commit

---
+++ rp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rp
+//
+//  Created by Marek Černý on 19/02/2017.
+//  Copyright © 2017 Marek Cerny. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
