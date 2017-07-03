ea352690fdcc510ce2c343a0d2246638ad6e2240
Marek Cerny <me@marekcerny.com>
2017-07-03 11:50:07 +0200

    Structural changes

--- rp/avoiders.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  avoiders.cpp
-//  rp
-//
-//  Created by Marek Černý on 01/07/2017.
-//  Copyright © 2017 Marek Cerny. All rights reserved.
-//
-
-#include "avoiders.hpp"
--- rp/permutation_set.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  permutation_set.cpp
-//  rp
-//
-//  Created by Marek Černý on 01/07/2017.
-//  Copyright © 2017 Marek Cerny. All rights reserved.
-//
-
-#include "permutation_set.hpp"
