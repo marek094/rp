e47b01b05c3dfb8c02f6b11132d12a62b6bdb177
Marek Cerny <me@marekcerny.com>
2017-11-11 18:41:32 +0100

    make function smaller

--- rp/bitwise/avoiders_parallel.hpp
+++ rp/bitwise/avoiders_parallel.hpp
@@ -40,9 +40,32 @@ namespace rp {
         return {cnt, size};
     }
     
+//    template <class PermutationSet>
+//    class buildAvoidersParallelCl {
+//        using Permutation = typename PermutationSet::Permutation;
+//        using Map = std::unordered_map<Permutation, unsigned, typename PermutationSet::Hash>;
+//    public:
+//        buildAvoidersParallel(PermutationSet&& patterns) : patterns{std::move(patterns)} {}
+//
+//        std::array<int, Permutation::MAX_SIZE> operator()() {
+//            return {}
+//        }
+//
+//
+//    private:
+//        Map avoiders, next_avoiders, others, next_others;
+//        std::array<int, Permutation::MAX_SIZE> sizes_cnt;
+//        unsigned bound, actual_size;
+//        // input
+//        PermutationSet patterns;
+//        // result
+//        std::array<int, Permutation::MAX_SIZE> result;
+//    };
+//
+    
     // given permutation and create extension maps
     template <class Permutation, class Map>
-    unsigned inline getNotAvoiders(const Map& avoiders, const Map& others,
+    inline unsigned getNotAvoiders(const Map& avoiders, const Map& others,
                             unsigned bound, unsigned size,
                             Permutation perm) {
         unsigned not_avoiders = 0;
@@ -58,14 +81,41 @@ namespace rp {
             not_avoiders |= not_avoider;
         }
         return not_avoiders;
-    };
+    }
     
+    template <class Permutation, class Func>
+    inline void walkSizeUpDown(Permutation perm, unsigned size, Func&& func) {
+        perm.up(0, size-1);
+        func((const Permutation)perm, 0);
+        for (int i = 1; i < size; ++i) {
+            perm.swapNext(i-1);
+            func((const Permutation)perm, i);
+        }
+    }
     
-    template <class PermutationSet, class Permutation = typename PermutationSet::Permutation>
+    template <class Permutation, class Map>
+    inline void saveNext(Map& next_avoiders, Map& next_others,
+                         unsigned not_avoiders, unsigned size,
+                         Permutation perm) {
+        walkSizeUpDown(perm, size, [&](auto&& perm, int i) mutable {
+            if ( !getBit(not_avoiders, i) ) {
+                auto ins = next_avoiders.insert({perm, not_avoiders});
+                assert(ins.second == true); // inserted new
+            } else {
+                auto ins = next_others.insert({perm, not_avoiders});
+                assert(ins.second == true); // inserted new
+            }
+        });
+    }
+    
+    template <
+        class PermutationSet,
+        class Permutation = typename PermutationSet::Permutation,
+        class Map = std::unordered_map<Permutation, unsigned, typename PermutationSet::Hash>
+    >
     std::array<int, Permutation::MAX_SIZE>
     buildAvoidersParallel(const PermutationSet& patterns) {
         
-        using Map = std::unordered_map<Permutation,  unsigned, typename PermutationSet::Hash>;
         Map avoiders, next_avoiders, others, next_others;
         std::array<int, Permutation::MAX_SIZE> sizes_cnt;
         unsigned bound = patterns.getBound();
@@ -79,7 +129,6 @@ namespace rp {
         }
         
         unsigned actual_size = 2;
-        
 
         for (; actual_size < patterns.getBound()+1; ++actual_size) {
             for (auto&& p : avoiders) {
@@ -101,58 +150,27 @@ namespace rp {
                     }
                     not_avoiders |= not_avoider;
                 }
-                Permutation perm2 = perm;
-                for (int i = 0; i < actual_size; ++i) {
-                    if (i > 0) {
-                        perm2.swapNext(i-1);
-                    }
-                    if (patterns.lookup(perm2)) {
+                walkSizeUpDown(p.first, actual_size, [&](auto&& perm, int i) mutable {
+                    if (patterns.lookup(perm)) {
                         setBit(not_avoiders, i);
                     }
-                }
-                
-                for (int i = 0; i < actual_size; ++i) {
-                    if (i > 0) perm.swapNext(i-1);
-                    if ( !getBit(not_avoiders, i) ) {
-                        auto ins = next_avoiders.insert({perm, not_avoiders});
-                        assert(ins.second == true); // inserted new
-                    } else {
-                        auto ins = next_others.insert({perm, not_avoiders});
-                        assert(ins.second == true); // inserted new
-                    }
-                }
+                });
+                saveNext(next_avoiders, next_others, not_avoiders, actual_size, p.first);
             }
             sizes_cnt[actual_size] = next_avoiders.size();
             
             avoiders.clear();
-            double coeff = (double)sizes_cnt[actual_size] / (double)sizes_cnt[actual_size-1];
-            double coeff2 = (double)next_others.size() / std::max(1.0,(double)others.size());
-            //                double coeff2 = 1.0; double coeff = 1.0;
-            avoiders.reserve( sizes_cnt[actual_size] * 5 * std::ceil(coeff) );
-            std::swap( avoiders, next_avoiders);
             others.clear();
-            others.reserve(   sizes_cnt[actual_size] * 5 * std::ceil(coeff2) );
+            std::swap( avoiders, next_avoiders);
             std::swap( others, next_others);
         }
         
-        
-        
         for (; actual_size < Permutation::MAX_SIZE-1; ++actual_size) {
+            
             for (auto&& p : avoiders) {
                 Permutation perm = p.first;
                 unsigned not_avoiders = getNotAvoiders(avoiders, others, bound, actual_size, perm);
-                for (int i = 0; i < actual_size; ++i) {
-                    if (i == 0) perm.up(0, actual_size-1);
-                    else perm.swapNext(i-1);
-                    if ( !getBit(not_avoiders, i) ) {
-                        auto ins = next_avoiders.insert({perm, not_avoiders});
-                        assert(ins.second == true); // inserted new
-                    } else {
-                        auto ins = next_others.insert({perm, not_avoiders});
-                        assert(ins.second == true); // inserted new
-                    }
-                    
-                }
+                saveNext(next_avoiders, next_others, not_avoiders, actual_size, p.first);
             }
             sizes_cnt[actual_size] = next_avoiders.size();
             
@@ -167,18 +185,17 @@ namespace rp {
             std::swap( others, next_others);
         }
         
-        
-        auto runTask = [&avoiders, &others, bound, actual_size] (auto begin, auto end) {
-            unsigned cnt = 0;
-            for (auto it = begin; it!=end; ++it) {
-                Permutation perm = *it;
-                unsigned not_avoiders = getNotAvoiders(avoiders, others, bound, actual_size, perm);
-                cnt += actual_size - __builtin_popcount(not_avoiders);
-            }
-            return cnt;
-        };
-        
         {   // Parallel part
+            auto runTask = [&avoiders, &others, bound, actual_size] (auto begin, auto end) {
+                unsigned cnt = 0;
+                for (auto it = begin; it!=end; ++it) {
+                    Permutation perm = *it;
+                    unsigned not_avoiders = getNotAvoiders(avoiders, others, bound, actual_size, perm);
+                    cnt += actual_size - __builtin_popcount(not_avoiders);
+                }
+                return cnt;
+            };
+            
             using namespace std;
             vector<Permutation> work;
             work.reserve(avoiders.size());
